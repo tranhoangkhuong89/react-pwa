349188230f64972aaf9fb9ce423482b3df8a5fdc
tranhoangkhuong89 <tranhoangkhuong89@gmail.com>
2020-05-07 16:18:04 +0700

    Updates

---
+++ precache-manifest.b7573c35becbc8f06966d9910989c5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e23bd76a73cc0996d3b22c5436fc3ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "471818160e20ea12af7e",
+    "url": "./static/css/main.29a18af4.chunk.css"
+  },
+  {
+    "revision": "5f2b2b919b4d2499e3f9",
+    "url": "./static/js/2.210df4b8.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "./static/js/2.210df4b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471818160e20ea12af7e",
+    "url": "./static/js/main.dc447f22.chunk.js"
+  },
+  {
+    "revision": "e4cee0bee21f645bd723",
+    "url": "./static/js/runtime-main.5a331893.js"
+  }
+]);
